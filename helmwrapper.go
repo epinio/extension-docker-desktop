440b2cf8a35cb2210a714e7c511a2bd4ee80e6ff
Mario Manno <mmanno@suse.com>
2022-03-24 11:53:27 +0100

    Fix windows path in helmwrapper

--- helmwrapper.go
+++ helmwrapper.go
@@ -7,30 +7,44 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 )
 
 // find out $0 path
 // switch working dir there
 // exec command
 func main() {
+	abs, err := os.Executable()
+	if err != nil {
+		log.Println("warning: cannot determine executables path")
+	}
+	abs = filepath.Dir(abs)
 
-	abs, _ := os.Executable()
-	abs = path.Dir(abs)
+	if abs == "" || abs == "." {
+		log.Println("warning: executables path is empty")
+	}
+
+	helm := path.Join(abs, "helm")
 
 	m := len(os.Args)
+	if m < 4 {
+		log.Println("usage: helmwrapper install release FILE")
+		os.Exit(1)
+	}
+
 	args := os.Args[1:m]
+	m = len(args)
+	args[m-1] = path.Join(abs, args[m-1])
 
-	args[m-1-1] = path.Join(abs, args[m-1-1])
-	fmt.Println(args)
-
-	command := exec.Command(path.Join(abs, "helm"), args...)
+	fmt.Printf("running: %s %v\n", helm, args)
+	command := exec.Command(helm, args...)
 
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 	command.Stdout = &stdout
 	command.Stderr = &stderr
 
-	err := command.Run()
+	err = command.Run()
 	if err != nil {
 		log.Println(err)
 	}
