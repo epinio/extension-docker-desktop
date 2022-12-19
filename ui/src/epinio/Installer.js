67cd44ab2bd460de2230753f507e867744ef9e9f
Tomas Hehejik <thehejik@suse.com>
2022-12-19 16:27:13 +0100

    Bump epinio to v1.5.2

--- ui/src/epinio/Installer.js
+++ ui/src/epinio/Installer.js
@@ -64,7 +64,7 @@ class EpinioInstaller extends React.Component {
         "--set", "global.domain=" + this.props.domain,
         "--set", "ingress.ingressClassName=nginx",
         "--set", "ingress.nginxSSLRedirect=false",
-        "https://github.com/epinio/helm-charts/releases/download/epinio-1.5.1/epinio-1.5.1.tgz"
+        "https://github.com/epinio/helm-charts/releases/download/epinio-1.5.2/epinio-1.5.2.tgz"
       ]);
       console.debug(JSON.stringify(result));
       console.log(result.stdout);
