48673d2c6eb69aa2cd1cd7fd6acf59df44edb0f3
Enrico Candino <enrico.candino@suse.com>
2023-04-05 12:39:45 +0200

    enable linter on all js files, add all failing rules

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -22,5 +22,29 @@ module.exports = {
     'react'
   ],
   rules: {
+    curly: 'off',
+    indent: 'off',
+    quotes: 'off',
+    semi: 'off',
+    'block-spacing': 'off',
+    'comma-dangle': 'off',
+    'dot-notation': 'off',
+    'keyword-spacing': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'no-tabs': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'no-useless-return': 'off',
+    'no-var': 'off',
+    'object-curly-spacing': 'off',
+    'object-property-newline': 'off',
+    'object-shorthand': 'off',
+    'padded-blocks': 'off',
+    'space-infix-ops': 'off',
+    'spaced-comment': 'off',
+    'space-before-function-paren': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
   }
 }
