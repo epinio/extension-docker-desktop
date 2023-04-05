3ea492299caeb728b4ad14ba832bc39425ad7da3
Enrico Candino <enrico.candino@gmail.com>
2023-04-05 13:12:16 +0200

    Enable linter on all js files (with disabled rules) (#32)
    * enable linter on all js files, add all failing rules
    
    * added more rules

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -22,5 +22,31 @@ module.exports = {
     'react'
   ],
   rules: {
+    curly: 'off',
+    indent: 'off',
+    quotes: 'off',
+    semi: 'off',
+    'block-spacing': 'off',
+    'comma-dangle': 'off',
+    'dot-notation': 'off',
+    'import/no-duplicates': 'off',
+    'keyword-spacing': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'no-multi-spaces': 'off',
+    'no-tabs': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    'no-useless-return': 'off',
+    'no-var': 'off',
+    'object-curly-spacing': 'off',
+    'object-property-newline': 'off',
+    'object-shorthand': 'off',
+    'padded-blocks': 'off',
+    'space-infix-ops': 'off',
+    'spaced-comment': 'off',
+    'space-before-function-paren': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
   }
 }
