1a3d545e4a0793b619414a06960a1614010ead0e
Enrico Candino <enrico.candino@gmail.com>
2023-04-05 11:35:58 +0200

    Added eslint linter (#31)
    * added linter
    
    * added linter

---
+++ ui/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  }
+}
