e3348dda21f540e797fe7e93987627b3ae8c5c57
Enrico Candino <enrico.candino@suse.com>
2023-05-08 18:19:44 +0200

    refactored Installer to functional approach, smaller functions, added check for Epinio release

--- ui/src/epinio/Installer.js
+++ ui/src/epinio/Installer.js
@@ -1,175 +1,205 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Delete as DeleteIcon, InstallDesktop as InstallDesktopIcon } from '@mui/icons-material'
-import { Box, Button, Card, CardActions, CardContent, LinearProgress, Typography } from '@mui/material'
+import { Box, Button, Card, CardActions, CardContent, CircularProgress, LinearProgress, Typography } from '@mui/material'
 
-class EpinioInstaller extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      progress: 0,
-      installation: false
+export default function EpinioInstaller({
+  hasKubernetes,
+  domain,
+  onInstallationChanged,
+  onError
+}) {
+  const [progress, setProgress] = useState(0)
+  const [installed, setInstalled] = useState(null)
+
+  async function helm(args) {
+    const result = await window.ddClient.extension.host.cli.exec('helm', args)
+    console.debug(JSON.stringify(result))
+
+    if (result.stderr) {
+      throw Error(result?.stderr)
     }
-    this.install = this.install.bind(this)
-    this.uninstall = this.uninstall.bind(this)
+
+    console.debug(result?.stdout)
   }
 
-  async helm(args) {
-    try {
-      return await window.ddClient.extension.host.cli.exec('helm', args)
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error(error.message)
-        throw error
-      } else {
-        console.error(JSON.stringify(error))
-        if (error.stderr) {
-          throw Error(error.stderr)
-        } else {
-          throw Error(JSON.stringify(error))
-        }
-      }
-    }
+  async function isEpinioInstalled() {
+    console.debug('checking if Epinio is already installed')
+
+    return helm(['status', '--namespace', 'epinio', 'epinio'])
+      .then(() => { setInstalled(true) })
+      .catch(() => { setInstalled(false) })
   }
 
-  async install() {
-    try {
-      console.log('installing NGINX chart')
-      this.setState({ progress: 10 })
-      let result = await this.helm([
-        'upgrade', '--install', '--atomic', 'ingress-nginx',
-        '--create-namespace', '--namespace', 'ingress-nginx',
-        'https://github.com/kubernetes/ingress-nginx/releases/download/helm-chart-4.3.0/ingress-nginx-4.3.0.tgz'
-      ])
-      console.debug(JSON.stringify(result))
-      console.log(result.stdout)
-      // https://github.com/docker/for-mac/issues/4903
-      console.log("installed: you might need to restart docker-desktop if localhost:443 doesn't forward to nginx")
-      this.setState({ progress: 25 })
+  async function install() {
+    installNginx()
+      .then(installCertManager)
+      .then(installEpinio)
+      .then(() => {
+        setInstalled(true)
+        onInstallationChanged(true)
+      })
+      .catch(error => {
+        setInstalled(false)
+        onInstallationChanged(false)
 
-      console.log('installing cert-manager chart')
-      this.setState({ progress: 30 })
-      result = await this.helm([
-        'upgrade', '--install', '--atomic', 'cert-manager',
-        '--create-namespace', '--namespace', 'cert-manager',
-        '--set', 'installCRDs=true',
-        '--set', 'extraArgs[0]=--enable-certificate-owner-ref=true',
-        'https://charts.jetstack.io/charts/cert-manager-v1.9.1.tgz'
-      ])
-      console.debug(JSON.stringify(result))
-      console.log(result.stdout)
-      console.log('installed: cert-manager')
-      this.setState({ progress: 50 })
-
-      console.log('installing Epinio chart')
-      this.setState({ progress: 55 })
-      result = await this.helm([
-        'upgrade', '--install', 'epinio',
-        '--create-namespace', '--namespace', 'epinio',
-        '--atomic',
-        '--set', 'global.domain=' + this.props.domain,
-        '--set', 'ingress.ingressClassName=nginx',
-        '--set', 'ingress.nginxSSLRedirect=false',
-        'https://github.com/epinio/helm-charts/releases/download/epinio-1.8.0/epinio-1.8.0.tgz'
-      ])
-      console.debug(JSON.stringify(result))
-      console.log(result.stdout)
-      console.log('installed: epinio')
-      this.setState({ progress: 100 })
-      this.props.onInstallationChanged(true)
-    } catch (error) {
-      this.props.onInstallationChanged(false)
-      const msg = 'If the nginx service is stuck in pending state, you might need to restart docker desktop.' + <br/> + error.message
-      this.props.onError(msg)
-    }
+        console.error(error)
+        const msg = 'If the nginx service is stuck in pending state, you might need to restart docker desktop.' + <br/> + JSON.stringify(error)
+        onError(msg)
+      })
+      .finally(() => {
+        setProgress(0)
+      })
   }
 
-  async uninstall() {
-    try {
-      console.log('uninstalling Epinio chart')
-      this.setState({ progress: 10 })
-      let result = await this.helm([
-        'uninstall', '--wait', 'epinio',
-        '--namespace', 'epinio'
-      ])
-      console.debug(JSON.stringify(result))
-      console.log(result.stdout)
-      console.log('installed: epinio')
-      this.setState({ progress: 25 })
-      this.props.onInstallationChanged(true)
-
-      console.log('uninstalling cert-manager chart')
-      this.setState({ progress: 30 })
-      result = await this.helm([
-        'uninstall', '--wait', 'cert-manager',
-        '--namespace', 'cert-manager'
-      ])
-      console.debug(JSON.stringify(result))
-      console.log(result.stdout)
-      console.log('uninstalled: cert-manager')
-      this.setState({ progress: 50 })
-
-      console.log('uninstalling NGINX chart')
-      this.setState({ progress: 75 })
-      result = await this.helm([
-        'uninstall', '--wait', 'ingress-nginx',
-        '--namespace', 'ingress-nginx'
-      ])
-      console.debug(JSON.stringify(result))
-      console.log(result.stdout)
-      // https://github.com/docker/for-mac/issues/4903
-      console.log('nginx successfully uninstalled')
-      this.setState({ progress: 100 })
-      this.props.onInstallationChanged(false)
-    } catch (error) {
-      this.props.onInstallationChanged(true)
-      const msg = 'If the nginx service is stuck in pending state, you might need to restart docker desktop.' + <br/> + error.message
-      this.props.onError(msg)
-    }
+  async function uninstall() {
+    uninstallEpinio()
+      .then(uninstallCertManager)
+      .then(uninstallNginx)
+      .then(() => {
+        setInstalled(false)
+        onInstallationChanged(true)
+      })
+      .catch(error => {
+        onInstallationChanged(false)
+        console.error(error)
+        onError(msg)
+      })
+      .finally(() => {
+        setProgress(0)
+      })
   }
 
-  render() {
-    // TODO install is idempotent, but maybe also detect working installation?
-    const progress = this.state.progress === 100 || this.state.progress === 0 ? null : <LinearProgress variant="determinate" value={this.state.progress} />
-    const hasKubernetes = this.props.hasKubernetes
-    const installation = this.props.installation
+  async function installNginx() {
+    console.log('installing nginx chart')
+    setProgress(10)
 
-    return (
-      <Card sx={{ height: '160px' }}>
-        <CardContent>
-          <Typography>
-            Install Epinio in Kubernetes
-          </Typography>
-          <br/>
-          <Typography variant="body2" align="left">
-          The Application Development Engine for Kubernetes.
-          Epinio uses build packs to create container images from source code.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button
-            startIcon={<InstallDesktopIcon/>}
-            variant="contained"
-            onClick={this.install}
-            // install button is disabled if kubernetes not available or Epinio already installed
-            disabled={!hasKubernetes || installation} >
-            Install/Upgrade
-          </Button>
-          <Button
-            startIcon={<DeleteIcon/>}
-            variant="contained"
-            onClick={this.uninstall}
-            // uninstall button is disabled if Epinio is not installed
-            disabled={!installation}
-            color="secondary" >
-            Uninstall
-          </Button>
-        </CardActions>
-        <Box sx={{ width: '100%' }}>
-          {progress}
-        </Box>
-      </Card>
-    )
+    await helm([
+      'upgrade', '--install', '--atomic', 'ingress-nginx',
+      '--create-namespace', '--namespace', 'ingress-nginx',
+      'https://github.com/kubernetes/ingress-nginx/releases/download/helm-chart-4.3.0/ingress-nginx-4.3.0.tgz'
+    ])
+
+    // https://github.com/docker/for-mac/issues/4903
+    console.log('installed: nginx')
+    console.log("you might need to restart docker-desktop if localhost:443 doesn't forward to nginx")
+    setProgress(25)
   }
+
+  async function installCertManager(result) {
+    console.log('installing cert-manager chart')
+    setProgress(30)
+
+    await helm([
+      'upgrade', '--install', '--atomic', 'cert-manager',
+      '--create-namespace', '--namespace', 'cert-manager',
+      '--set', 'installCRDs=true',
+      '--set', 'extraArgs[0]=--enable-certificate-owner-ref=true',
+      'https://charts.jetstack.io/charts/cert-manager-v1.9.1.tgz'
+    ])
+
+    console.log('installed: cert-manager')
+    setProgress(50)
+  }
+
+  async function installEpinio() {
+    console.log('installing epinio chart')
+    setProgress(55)
+
+    await helm([
+      'upgrade', '--install', 'epinio',
+      '--create-namespace', '--namespace', 'epinio',
+      '--atomic',
+      '--set', 'global.domain=' + domain,
+      '--set', 'ingress.ingressClassName=nginx',
+      '--set', 'ingress.nginxSSLRedirect=false',
+      'https://github.com/epinio/helm-charts/releases/download/epinio-1.8.0/epinio-1.8.0.tgz'
+    ])
+
+    console.log('installed: epinio')
+    setProgress(100)
+  }
+
+  async function uninstallEpinio() {
+    console.log('uninstalling epinio chart')
+    setProgress(10)
+
+    await helm([
+      'uninstall', '--namespace', 'epinio',
+      '--wait', 'epinio'
+    ])
+
+    console.log('uninstalled: epinio')
+    setProgress(25)
+  }
+
+  async function uninstallCertManager() {
+    console.log('uninstalling cert-manager chart')
+    setProgress(30)
+
+    await helm([
+      'uninstall', '--namespace', 'cert-manager',
+      '--wait', 'cert-manager'
+    ])
+
+    console.log('uninstalled: cert-manager')
+    setProgress(50)
+  }
+
+  async function uninstallNginx() {
+    console.log('uninstalling nginx chart')
+    setProgress(75)
+
+    await helm([
+      'uninstall', '--namespace', 'ingress-nginx',
+      '--wait', 'ingress-nginx'
+    ])
+
+    console.log('uninstalled: nginx')
+    setProgress(100)
+  }
+
+  if (installed === null) {
+    isEpinioInstalled()
+  }
+
+  // TODO install is idempotent, but maybe also detect working installation?
+  const isLoading = progress !== 100 && progress !== 0
+
+  return (
+    <Card sx={{ height: '160px' }}>
+      <CardContent>
+        <Typography>
+          Install Epinio in Kubernetes
+        </Typography>
+        <br/>
+        <Typography variant="body2" align="left">
+        The Application Development Engine for Kubernetes.
+        Epinio uses build packs to create container images from source code.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          startIcon={<InstallDesktopIcon/>}
+          variant="contained"
+          onClick={install}
+          // install button is disabled if kubernetes not available or Epinio already installed
+          disabled={!!installed || isLoading || !hasKubernetes} >
+          Install/Upgrade
+        </Button>
+        <Button
+          startIcon={<DeleteIcon/>}
+          variant="contained"
+          onClick={uninstall}
+          // uninstall button is disabled if Epinio is not installed
+          disabled={!installed || isLoading}
+          color="secondary" >
+          Uninstall
+        </Button>
+        {isLoading && <CircularProgress style={{ width: '20px', height: '20px', marginLeft: '10px' }} />}
+      </CardActions>
+      <Box sx={{ width: '100%' }}>
+        {isLoading && <LinearProgress variant="determinate" value={progress} />}
+      </Box>
+    </Card>
+  )
 }
-
-export default EpinioInstaller
