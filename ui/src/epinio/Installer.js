64b94a19a1e16f54829eb38c36b7d686ae4ac94b
Tomas Hehejik <thehejik@suse.com>
2023-07-20 13:08:44 +0200

    manual bump to E 1.9.0-rc5

--- ui/src/epinio/Installer.js
+++ ui/src/epinio/Installer.js
@@ -140,7 +140,7 @@ export default function EpinioInstaller({
       '--set', 'ingress.ingressClassName=nginx',
       '--set', 'ingress.nginxSSLRedirect=false',
       '--set', 'server.accessControlAllowOrigin=*',
-      'https://github.com/epinio/helm-charts/releases/download/epinio-1.8.1/epinio-1.8.1.tgz'
+      'https://github.com/epinio/helm-charts/releases/download/epinio-1.9.0-rc5/epinio-1.9.0-rc5.tgz'
     ])
 
     console.log('installed: epinio')
